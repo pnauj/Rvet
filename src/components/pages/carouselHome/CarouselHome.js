dc5ed8f20ef14736a7ac5cd48e85140cdc8c7ce9
pnauj <torres.jpw@gmail.com>
2022-07-09 16:38:26 -0300

    se empezo con el maquetado del proyecto

---
+++ src/components/pages/carouselHome/CarouselHome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel
+
+const CarouselHome = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CarouselHome;

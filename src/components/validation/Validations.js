64dd45efd837547c1386e84015cc6c7ac443bcf1
pnauj <torres.jpw@gmail.com>
2022-07-16 14:23:27 -0300

    se crearon expresiones regulares para las validaciones

--- src/components/validation/Validations.js
+++ src/components/validation/Validations.js
@@ -1,10 +1,12 @@
-const erTexto = /^[A-Za-z\s?]+$/;
+const erNombre = /^[A-Za-z\s?]+$/;
 const erVet = /^[A-Za-z\s?]+$/;
+const erFecha = /^(?:3[01]|[12][0-9]|0?[1-9])([\-/.])(0?[1-9]|1[1-2])\1\d{4}$/;
+const erHora = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
 
 
 
-export const validateTexto = (campo) => {
-    if (erTexto.test(campo) && campo.trim().length < 45 && campo.trim() !== "") {
+export const validateNombre = (campo) => {
+    if (erNombre.test(campo) && campo.trim().length < 45 && campo.trim() !== "") {
       return true;
     } else {
       return false;

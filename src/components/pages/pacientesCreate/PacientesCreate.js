7222df42e44c9f1fd9d3772ad08f2a7c5e5d98bc
pnauj <torres.jpw@gmail.com>
2022-07-21 12:09:52 -0300

    se empezo a crear la logica con formik para el formulario de pacientes

--- src/components/pages/pacientesCreate/PacientesCreate.js
+++ src/components/pages/pacientesCreate/PacientesCreate.js
@@ -9,30 +9,62 @@ const PacientesCreate = () => {
         <h1>Agregar Pacientes</h1>
         <hr />
         <Formik
-          onSubmit={() => {
+          initialValues={{
+            Nombre: '',
+            Apellido: '',
+            Email: '',
+            Telefono: '',
+            NombreMascota: '',
+            Especie: '',
+            Raza: ''
+          }}
+
+          onSubmit={(valores) => {
+            console.log(valores);
             console.log("formulario enviado");
           }}
         >
-          {() => (
-            <Form className="my-5">
+          {({values,handleChange, handleSubmit, handleBlur}) => (
+            <Form className="my-5" onSubmit={handleSubmit}>
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Nombre</Form.Label>
-                <Form.Control type="text" placeholder="Escriba su nombre" />
+                <Form.Control 
+                type="text" 
+                placeholder="Escriba su nombre"
+                value={values.Nombre}
+                onChange={handleChange}
+                onBlur={handleBlur} 
+                />
               </Form.Group>
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Apellido</Form.Label>
-                <Form.Control type="text" placeholder="Escriba su Apellido" />
+                <Form.Control 
+                type="text" 
+                placeholder="Escriba su Apellido"
+                value={values.Apellido}
+                onChange={handleChange}
+                onBlur={handleBlur} 
+                />
               </Form.Group>
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Email</Form.Label>
                 <Form.Control
                   type="email"
                   placeholder="Ej: ejemplo@correo.com"
+                  value={values.Email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
                 />
               </Form.Group>
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Telefono</Form.Label>
-                <Form.Control type="number" placeholder="Escriba su telefono" />
+                <Form.Control 
+                type="number" 
+                placeholder="Escriba su telefono"
+                value={values.Telefono} 
+                onChange={handleChange}
+                onBlur={handleBlur}
+                />
               </Form.Group>
               <hr />
               <h3>Datos de la Mascotas</h3>
@@ -41,11 +73,18 @@ const PacientesCreate = () => {
                 <Form.Control
                   type="text"
                   placeholder="Escriba el nombre de mascota"
+                  value={values.NombreMascota}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
                 />
               </Form.Group>
               <Form.Group className="mb-3" controlId="formBasicCheckbox">
                 <Form.Label>Especie</Form.Label>
-                <Form.Select>
+                <Form.Select
+                value={values.Especie}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                >
                   <option value="">Elija una opcion</option>
                   <option value="Perro">Perro</option>
                   <option value="Gato">Gato</option>
@@ -58,10 +97,13 @@ const PacientesCreate = () => {
                 <Form.Control
                   type="text"
                   placeholder="Escriba la raza de su mascota"
+                  value={values.Raza}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
                 />
               </Form.Group>
               <div className="text-end">
-                <Button className="btn-yellow">Guardar</Button>
+                <Button type="submit" className="btn-yellow">Guardar</Button>
               </div>
             </Form>
           )}
@@ -72,3 +114,5 @@ const PacientesCreate = () => {
 };
 
 export default PacientesCreate;
+
+//video formik 00:28:00
